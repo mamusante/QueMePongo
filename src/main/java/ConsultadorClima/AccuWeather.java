11479cfd746fc1c6016edfb57fa5a81d8faee45f
Maria Musante <mmusante@frba.utn.edu.ar>
2025-06-03 22:25:23 -0300

    agrego logica de max 10 consultas

--- src/main/java/ConsultadorClima/AccuWeather.java
+++ src/main/java/ConsultadorClima/AccuWeather.java
@@ -1,6 +1,9 @@
 package ConsultadorClima;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+
 import Exceptions.TemperaturaNoValidaException;
 
 import AccuWeatherApi.AccuWeatherAPI;
@@ -8,10 +11,15 @@ import AccuWeatherApi.AccuWeatherAPI;
 public class AccuWeather implements ConsultadorClima {
 
   AccuWeatherAPI api = new AccuWeatherAPI();
+  private int consultasRealizadasHoy = 1; //arranca en 1 porque al isntanciar la clase se hace una consulta
+  private LocalDate fechaUltimaConsulta = LocalDate.now();
+  List<Map<String, Object>> datos = api.getWeather("Buenos Aires, Argentina");
 
   @Override
   public double getTemperaturaActual() {
-    List<Map<String, Object>> datos = api.getWeather("Buenos Aires, Argentina");
+
+    consultarApiSiEsPosible();
+
     if (datos.isEmpty()) throw new TemperaturaNoValidaException("No se puede acceder a los datos de temp de BS AS");
 
     Map<String, Object> primerDato = datos.get(0);
@@ -28,4 +36,22 @@ public class AccuWeather implements ConsultadorClima {
     return (valor - 32) * 5.0 / 9.0;
   }
 
+  private void consultarApiSiEsPosible() {
+    LocalDate hoy = LocalDate.now();
+
+    if (!hoy.equals(fechaUltimaConsulta)) {
+      consultasRealizadasHoy = 0;
+      fechaUltimaConsulta = hoy;
+    }
+
+    if (consultasRealizadasHoy < 10) {
+      // Solo consultás si no pasaste el límite
+      datos = api.getWeather("Buenos Aires, Argentina");
+      consultasRealizadasHoy++;
+    }
+  }
+
+
 }
+
+
