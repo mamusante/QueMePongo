c65fcb686eec1d03e644b7610e2878ff24aed3ea
Maria Musante <mmusante@frba.utn.edu.ar>
2025-06-08 15:47:05 -0300

    agrego logica de cache para el accuweather

--- src/main/java/ConsultadorClima/AccuWeather.java
+++ src/main/java/ConsultadorClima/AccuWeather.java
@@ -1,5 +1,8 @@
 package ConsultadorClima;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -11,18 +14,19 @@ import AccuWeatherApi.AccuWeatherAPI;
 public class AccuWeather implements ConsultadorClima {
 
   AccuWeatherAPI api = new AccuWeatherAPI();
-  private int consultasRealizadasHoy = 1; //arranca en 1 porque al isntanciar la clase se hace una consulta
-  private LocalDate fechaUltimaConsulta = LocalDate.now();
-  List<Map<String, Object>> datos = api.getWeather("Buenos Aires, Argentina");
+  private List<Map<String, Object>> ultimaRespuesta;
+  private LocalDateTime proximaExpiracion;
+  private Duration expiracion = Duration.ofHours(2).plusMinutes(30);
 
   @Override
   public double getTemperaturaActual() {
 
-    consultarApiSiEsPosible();
+    if (this.ultimaRespuesta == null || this.expiro()) {
+      this.ultimaRespuesta = consultarApi();
+      this.proximaExpiracion = LocalDateTime.now().plus(this.expiracion);
+    }
 
-    if (datos.isEmpty()) throw new TemperaturaNoValidaException("No se puede acceder a los datos de temp de BS AS");
-
-    Map<String, Object> primerDato = datos.get(0);
+    Map<String, Object> primerDato = ultimaRespuesta.get(0);
     Map<String, Object> temperatura = (Map<String, Object>) primerDato.get("Temperature");
 
 
@@ -30,28 +34,22 @@ public class AccuWeather implements ConsultadorClima {
     valor = pasarACelsius(valor);
 
     return valor;
-  }
+
+    }
+
+
+
+private boolean expiro() {
+  return proximaExpiracion.isAfter(LocalDateTime.now());
+}
 
   private double pasarACelsius(double valor){
     return (valor - 32) * 5.0 / 9.0;
   }
 
-  private void consultarApiSiEsPosible() {
-    LocalDate hoy = LocalDate.now();
-
-    if (!hoy.equals(fechaUltimaConsulta)) {
-      consultasRealizadasHoy = 0;
-      fechaUltimaConsulta = hoy;
-    }
-
-    if (consultasRealizadasHoy < 10) {
-      // Solo consultás si no pasaste el límite
-      datos = api.getWeather("Buenos Aires, Argentina");
-      consultasRealizadasHoy++;
-    }
+  private List<Map<String, Object>> consultarApi(){
+    return api.getWeather("Buenos Aires, Argentina");
   }
-
-
 }
 
 
